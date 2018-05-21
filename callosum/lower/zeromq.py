496e1c6e54fb86876427027f07e5fd41fbc4118c
Joongi Kim <me@daybreaker.info>
2018-05-21 17:29:18 +0900

    lower.zeromq: Make socket options customizable

--- callosum/lower/zeromq.py
+++ callosum/lower/zeromq.py
@@ -37,8 +37,9 @@ class ZeroMQBinder(AbstractBinder):
         # server_key = zmq.auth.load_certificate('test-server.key_secret')
         # sock.setsockopt(zmq.CURVE_SERVER, 1)
         # sock.setsockopt(zmq.CURVE_SECRETKEY, server_key[1])
-        pull_sock.setsockopt(zmq.LINGER, 100)
-        push_sock.setsockopt(zmq.LINGER, 100)
+        for key, value in self.transport._zsock_opts.items():
+            pull_sock.setsockopt(key, value)
+            push_sock.setsockopt(key, value)
         pull_sock.bind(self.addr)
         url = yarl.URL(self.addr)
         push_sock.bind(str(url.with_port(url.port + 1)))
@@ -47,10 +48,7 @@ class ZeroMQBinder(AbstractBinder):
         return ZeroMQConnection(self.transport)
 
     async def __aexit__(self, exc_type, exc_obj, exc_tb):
-        self.transport._pull_sock.close()
-        self.transport._push_sock.close()
-        self.transport._pull_sock = None
-        self.transport._push_sock = None
+        pass
 
 
 class ZeroMQConnector(AbstractConnector):
@@ -67,8 +65,9 @@ class ZeroMQConnector(AbstractConnector):
         # sock.setsockopt(zmq.CURVE_SERVERKEY, server_key[0])
         # sock.setsockopt(zmq.CURVE_PUBLICKEY, client_key[0])
         # sock.setsockopt(zmq.CURVE_SECRETKEY, client_key[1])
-        pull_sock.setsockopt(zmq.LINGER, 100)
-        push_sock.setsockopt(zmq.LINGER, 100)
+        for key, value in self.transport._zsock_opts.items():
+            pull_sock.setsockopt(key, value)
+            push_sock.setsockopt(key, value)
         push_sock.connect(self.addr)
         url = yarl.URL(self.addr)
         pull_sock.connect(str(url.with_port(url.port + 1)))
@@ -82,11 +81,25 @@ class ZeroMQConnector(AbstractConnector):
 
 class ZeroMQTransport(BaseTransport):
 
+    '''
+    Implementation for the ZeorMQ-backed transport.
+
+    It keeps a single persistent connection over multiple connections.
+    As the underlying PUSH/PULL sockets work asynchronously, this
+    effectively achieves connection pooling reducing handshake overheads.
+    '''
+
     binder_cls = ZeroMQBinder
     connector_cls = ZeroMQConnector
 
-    def __init__(self):
+    def __init__(self, **kwargs):
+        self._zsock_opts = {
+            zmq.LINGER: 100,
+            **kwargs.pop('zsock_opts', {}),
+        }
+        super().__init__(**kwargs)
         self._zctx = zmq.asyncio.Context()
+        # Keep sockets during the transport lifetime.
         self._pull_sock = None
         self._push_sock = None
 
