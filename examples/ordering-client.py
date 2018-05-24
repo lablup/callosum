0104db7ad75ef399e29966495a2bb1c54c3827dd
Joongi Kim <me@daybreaker.info>
2018-05-24 11:29:54 +0900

    examples: Add an example to show effects of ordering keys

---
+++ examples/ordering-client.py
@@ -0,0 +1,44 @@
+import asyncio
+import json
+
+from callosum import Peer
+
+
+async def call():
+    peer = Peer(connect='tcp://localhost:5000',
+                serializer=json.dumps,
+                deserializer=json.loads,
+                invoke_timeout=5.0)
+    await peer.open()
+
+    print('Check the server log to see in which order echo/add are executed.\n')
+
+    print('== Calling with the same ordering key ==')
+    tasks = [
+        peer.invoke('echo', {'sent': 'bbbb'}, order_key='mykey'),
+        peer.invoke('add', {'a': 1, 'b': 2}, order_key='mykey'),
+    ]
+    responses = await asyncio.gather(*tasks)
+    print(responses)
+
+    await peer.invoke('print_delim', {})
+
+    print('== Calling without any ordering key ==')
+    tasks = [
+        peer.invoke('echo', {'sent': 'bbbb'}),
+        peer.invoke('add', {'a': 1, 'b': 2}),
+    ]
+    responses = await asyncio.gather(*tasks)
+    print(responses)
+
+    await peer.invoke('print_delim', {})
+
+    await peer.close()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(call())
+    finally:
+        loop.close()
---
+++ examples/ordering-server.py
@@ -0,0 +1,61 @@
+import asyncio
+import json
+
+from callosum import Peer
+
+
+async def handle_echo(request):
+    print('echo start')
+    await asyncio.sleep(1)
+    print('echo done')
+    return {
+        'received': request.body['sent'],
+    }
+
+
+async def handle_add(request):
+    print('add start')
+    await asyncio.sleep(0.5)
+    print('add done')
+    return {
+        'result': request.body['a'] + request.body['b'],
+    }
+
+
+async def handle_delimeter(request):
+    print('------')
+
+
+async def serve():
+    # Peer will use "KeySerializedAsyncScheduler" by default.
+    peer = Peer(bind='tcp://127.0.0.1:5000',
+                serializer=json.dumps,
+                deserializer=json.loads)
+    peer.handle_function('echo', handle_echo)
+    peer.handle_function('add', handle_add)
+    peer.handle_function('print_delim', handle_delimeter)
+
+    print('echo() will take 1 second and add() will take 0.5 second.')
+    print('You can confirm the effect of scheduler and the ordering key by the '
+          'console logs.\n')
+
+    try:
+        await peer.open()
+        print('listening...')
+        await peer.listen()
+    except asyncio.CancelledError:
+        await peer.close()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        task = loop.create_task(serve())
+        loop.run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        print('closing...')
+        task.cancel()
+        loop.run_until_complete(task)
+    finally:
+        loop.close()
+        print('closed.')
