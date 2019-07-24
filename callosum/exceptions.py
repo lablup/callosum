4b86656315940abfd09790737d103b224ee8ad76
MyrzakhmetovTemirlan <temirlanmirzakhmetov@gmail.com>
2019-07-24 15:02:03 +0900

    Add custom ParamError

--- callosum/exceptions.py
+++ callosum/exceptions.py
@@ -6,6 +6,17 @@ class ClientError(CallosumError):
     pass
 
 
+class ParamError(ClientError):
+
+    def __init__(self, error_param: str):
+        self.message =\
+            f'''
+            {error_param} must not be specified in RedisStreamAddress,\
+                 as objects using CommonStreamBinder are not supposed\
+                      to be the consumers of any group.
+            '''
+
+
 class ServerError(CallosumError):
     pass
 
