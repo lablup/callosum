eac3971a62591ad8106c8e32a3b509217ba14e99
MyrzakhmetovTemirlan <temirlanmirzakhmetov@gmail.com>
2019-07-24 14:32:50 +0900

    Add CommonStreamBinder & CommonStreamConnector classes and modify some parts of the code accordingly

--- callosum/lower/redis.py
+++ callosum/lower/redis.py
@@ -12,6 +12,7 @@ from . import (
     AbstractConnection,
     BaseTransport,
 )
+from ..exceptions import ClientError
 # from ..auth import Identity
 # from ..compat import current_loop
 
@@ -20,8 +21,8 @@ from . import (
 class RedisStreamAddress(AbstractAddress):
     redis_server: Union[str, Tuple[str, int]]
     stream_key: str
-    group: Optional[str] = None     # only required by receiver-side
-    consumer: Optional[str] = None  # only required by receiver-side
+    group: Optional[str] = None     # only required by receiver-side; must not be specified for CommonStreamBinder
+    consumer: Optional[str] = None  # only required by receiver-side; must not be specified for CommonStreamBinder
 
 
 class RedisStreamConnection(AbstractConnection):
@@ -34,7 +35,7 @@ class RedisStreamConnection(AbstractConnection):
 
     def __init__(self, transport: RedisStreamTransport,
                  addr: RedisStreamAddress,
-                 direction_keys: Tuple[str, str]):
+                 direction_keys: Optional[Tuple[str, str]] = None):
         self.transport = transport
         self.addr = addr
         self.direction_keys = direction_keys
@@ -42,7 +43,10 @@ class RedisStreamConnection(AbstractConnection):
     async def recv_message(self) -> AsyncGenerator[
             Optional[Tuple[bytes, bytes]], None]:
         # assert not self.transport._redis.closed
-        stream_key = f'{self.addr.stream_key}.{self.direction_keys[0]}'
+        if not self.direction_keys:
+            stream_key = self.addr.stream_key
+        else:
+            stream_key = f'{self.addr.stream_key}.{self.direction_keys[0]}'
         _s = asyncio.shield
 
         async def _xack(raw_msg):
@@ -68,10 +72,88 @@ class RedisStreamConnection(AbstractConnection):
 
     async def send_message(self, raw_msg: Tuple[bytes, bytes]) -> None:
         # assert not self.transport._redis.closed
-        stream_key = f'{self.addr.stream_key}.{self.direction_keys[1]}'
+        if not self.direction_keys:
+            stream_key = self.addr.stream_key
+        else:
+            stream_key = f'{self.addr.stream_key}.{self.direction_keys[1]}'
         _s = asyncio.shield
         await _s(self.transport._redis.xadd(
             stream_key, {b'hdr': raw_msg[0], b'msg': raw_msg[1]}))
+
+
+class CommonStreamBinder(AbstractBinder):
+    '''
+    CommonStreamBinder is for use with Publisher class.
+    All Publishers using CommonStreamBinder are supposed to provide the same stream key for the purposes
+    of subsequent message load-balancing among those, who read messages from the stream (Subscribers).
+    '''
+
+    __slots__ = ('transport', 'addr')
+
+    transport: RedisStreamTransport
+    addr: RedisStreamAddress
+
+    async def __aenter__(self):
+        self.transport._redis = await aioredis.create_redis(
+            self.addr.redis_server,
+            **self.transport._redis_opts)
+        key = self.addr.stream_key
+        #If there were no stream with the specified key before, it is created as a side effect of adding the message.
+        await self.transport._redis.xadd(key, {b'meta': b'create-or-join-to-stream'})
+        if self.addr.group:
+            raise ClientError("Group must not be specified in RedisStreamAddress, as objects\
+                using CommonStreamBinder are not supposed to be the consumers of any group.")
+        if self.addr.consumer:
+            raise ClientError("Consumer must not be specified in RedisStreamAddress, as objects\
+                using CommonStreamBinder are not supposed to be the consumers of any group.")
+        return RedisStreamConnection(self.transport, self.addr)
+
+    async def __aexit__(self, exc_type, exc_obj, exc_tb):
+        pass
+
+
+class CommonStreamConnector(AbstractConnector):
+    '''
+    CommonStreamConnector is for ise with Subscriber class.
+    All Subscribers using CommonStreamConnector are supposed to provide the same stream key and same group name
+    for the purposes of subsequent load-balancing. Based on the consumer group name, RedisStream will make sure
+    that each subscriber from the group gets distinct set of messages.
+    '''
+
+    __slots__ = ('transport', 'addr')
+
+    transport: RedisStreamTransport
+    addr: RedisStreamAddress
+
+    async def __aenter__(self):
+        pool = await aioredis.create_connection(
+            self.addr.redis_server,
+            **self.transport._redis_opts)
+        self.transport._redis = aioredis.Redis(pool)
+        key = self.addr.stream_key
+        #If there were no stream with the specified key before, it is created as a side effect of adding the message.
+        await self.transport._redis.xadd(key, {b'meta': b'create-or-join-to-stream'})
+        groups = await self.transport._redis.xinfo_groups(key)
+        if not any(map(lambda g: g[b'name'] == self.addr.group.encode(), groups)):
+            await self.transport._redis.xgroup_create(
+                key, self.addr.group)
+        return RedisStreamConnection(self.transport, self.addr)
+
+    async def __aexit__(self, exc_type, exc_obj, exc_tb):
+        # we need to create a new Redis connection for cleanup
+        # because self.transport._redis gets corrupted upon
+        # cancellation of Peer._recv_loop() task.
+        _redis = await aioredis.create_redis(
+            self.addr.redis_server,
+            **self.transport._redis_opts)
+        try:
+            await asyncio.shield(_redis.xgroup_delconsumer(
+                self.addr.stream_key,
+                self.addr.group,
+                self.addr.consumer))
+        finally:
+            _redis.close()
+            await _redis.wait_closed()
 
 
 class RedisStreamBinder(AbstractBinder):
