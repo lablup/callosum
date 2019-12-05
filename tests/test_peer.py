baae53ef3338cfc8aaa06c9098a2a85135bfcd36
Joongi Kim <joongi@lablup.com>
2019-12-05 22:52:41 +0900

    Remove tests for removed parts

--- tests/test_peer.py
+++ tests/test_peer.py
@@ -18,7 +18,6 @@ async def test_init():
     assert p._invoke_timeout is None or p._invoke_timeout > 0
     assert p._exec_timeout is None or p._exec_timeout > 0
     assert len(p._func_registry) == 0
-    assert len(p._stream_registry) == 0
 
 
 @pytest.mark.asyncio
@@ -32,26 +31,6 @@ async def test_func_registry():
         pass
 
     assert 'dummy' not in p._func_registry
-    assert 'dummy' not in p._stream_registry
     p.handle_function('dummy', dummy)
     assert 'dummy' in p._func_registry
-    assert 'dummy' not in p._stream_registry
     assert p._func_registry['dummy'] is dummy
-
-
-@pytest.mark.asyncio
-async def test_stream_registry():
-    p = Peer(connect=ZeroMQAddress('tcp://127.0.0.1:5000'),
-             serializer=json.dumps,
-             deserializer=json.loads,
-             transport=ZeroMQRPCTransport)
-
-    def dummy():
-        pass
-
-    assert 'dummy' not in p._func_registry
-    assert 'dummy' not in p._stream_registry
-    p.handle_stream('dummy', dummy)
-    assert 'dummy' not in p._func_registry
-    assert 'dummy' in p._stream_registry
-    assert p._stream_registry['dummy'] is dummy
