c281e309871fa270e240d82f529bc58446043ac4
Joongi Kim <joongi@lablup.com>
2020-06-01 16:55:19 +0900

    fix: Improve RPC error/cancellation handling
    * Previously cancellation in the server-side has been silently
      swallowed and the client's resolver never returned.
      Added a new "CANCELLED" message type to resolve this issue.
    
    * Make the ordering module type-safe, using enum-based sentinel
      types.
    
    * Drop use of aiojobs since we already do the scheduling.
    
    * Simplify implementation of ExitOrderedAsyncScheduler.
    
    * setup: Update aiotools to the latest version.

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ def read_src_version():
 
 
 install_requires = [
-    'aiojobs>=0.2.2',
-    'aiotools>=0.8.5',
+    'aiotools>=0.9.1',
     'async_timeout>=3.0.1',
     'attrs>=19.3.0',
     'python-dateutil>=2.8.1',
